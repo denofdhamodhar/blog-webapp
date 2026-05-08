c35d6b45b0308805ba6e8da2f66c2343c80e825a
denofdhamodhar <dhamodhar459@gmail.com>
2026-05-08 23:03:23 +0530

    button component completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,20 @@
-
-function Button() {
+function Button({
+  label,
+  className,
+  bgColor = "bg-black",
+  textColor = "text-white",
+  type,
+  ...props
+}) {
   return (
-    <div>Button</div>
-  )
+    <button
+      type={type}
+      {...props}
+      className={`${className} ${bgColor} ${textColor} px-4 py-1.5 inline-block rounded-full font-medium font-paragraph`}
+    >
+      {label}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
