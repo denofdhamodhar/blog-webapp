758b8d97c0491e19f426d2a3aff097ae8e623aed
denofdhamodhar <dhamodhar459@gmail.com>
2026-05-08 20:39:01 +0530

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Outlet />
       </div>
+      {/* <Footer /> */}
     </div>
   );
 }
